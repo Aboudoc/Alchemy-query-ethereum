021aef93bf99c5f683d55c5ec35514b4ef953efc
Aboudoc <aboutika.reda@gmail.com>
2022-12-11 06:45:38 +0100

    initial commit

--- index.js
+++ index.js
@@ -17,3 +17,23 @@ axios
   .then((response) => {
     console.log(response.data.result);
   });
+
+/**
+   * const axios = require("axios");
+
+// copy-paste your URL provided in your Alchemy.com dashboard
+const ALCHEMY_URL =
+  "https://eth-mainnet.g.alchemy.com/v2/kfwwkeZ77xqeyzaQqXSLgMpUpMOYfk4I";
+
+axios
+  .post(ALCHEMY_URL, {
+    jsonrpc: "2.0",
+    id: 1,
+    method: "eth_getBlockTransactionCountByNumber",
+    params: "finalized",
+  })
+  .then((response) => {
+    console.log(response.data.result);
+  });
+
+   */
